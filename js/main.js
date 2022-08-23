b7f353354a6c455276ceeb1e68e9e145b67b4920
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-08-22 23:13:10 -0300

    Crea constantes para llamar elementos del .HTML

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const $form = document.querySelector('#carta-a-santa');
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcion = $form['descripcion-regalo'].value;
+
