85c10bca74d76ea7351a9a2a3f8eb16449e3d179
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-08-22 23:14:22 -0300

    Crea funciones para validar datos del formulario

--- js/main.js
+++ js/main.js
@@ -5,3 +5,34 @@ const ciudad = $form.ciudad.value;
 const comportamiento = $form.comportamiento.value;
 const descripcion = $form['descripcion-regalo'].value;
 
+function validarNombre(nombre) {
+    if(nombre.length === 0) {
+        return 'Este campo debe tener al menos 1 caracter';
+    }
+
+    if(nombre.length >= 50) {
+        return 'Este campo debe tener menos de 50 caracteres'
+    }
+
+    return '';
+}
+
+function validarCiudad(ciudad) {
+    if(ciudad.length === 0) {
+        return 'Este campo debe tener una ciudad seleccionada'
+    }
+
+    return '';
+}
+
+function validarDescripcion(descripcion) {
+    if(descripcion.length === 0) {
+        return 'Este campo debe tener al menos 1 caracter';
+    }
+
+    if(descripcion.length >= 100) {
+        return 'Este campo debe tener menos de 100 caracteres'
+    }
+
+    return '';
+}
