95a8b058e9b83f6af0482c8a2385cd6f4ed2be93
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-08-23 23:44:39 -0300

    Actualiza pruebas
    Mejora algunos textos y nombres de variables y/o funciones, además añade pruebas en caso de que la función corra perfectamente

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,34 +1,62 @@
 function probarValidarNombre() {
-    console.assert(
-    validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-    'validar nombre no validó que el nombre sea vacío'
-);
 
-console.assert(
-    validarNombre('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') === 'Este campo debe tener menos de 50 caracteres',
-    'validar nombre no validó que el nombre sea menor a 50 caracteres'
-);
+    console.assert(
+        validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+        'validar nombre no validó que el nombre sea vacío'
+    );
+
+    console.assert(
+        validarNombre('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') === 'Este campo debe tener menos de 50 caracteres',
+       'validar nombre no validó que el nombre sea menor a 50 caracteres'
+    );
+
+    console.assert(
+      validarNombre('123123') === 'Este campo sólo puede tener letras',
+        'validar nombre no validó que el nombre sólo contenga letras'
+    );
+
+    console.assert(
+        validarNombre('Mipmipp') === '',
+        'validar nombre no validó que el nombre esté bien'
+    );
 }
 
 function probarValidarCiudad() {
+
     console.assert(
         validarCiudad('') === 'Este campo debe tener una ciudad seleccionada',
         'validar ciudad no validó que no haya una ciudad seleccionada'
-    )
+    );
+
+    console.assert(
+        validarCiudad('Buenos Aires') === '',
+        'validar ciudad no validó que la ciudad esté bien seleccionada'
+    );
 }
 
-function probarValidarDescripcion() {
+function probarValidarDescripcionRegalo() {
+    
     console.assert(
-        validarDescripcion('') === 'Este campo debe tener al menos 1 caracter',
+        validarDescripcionRegalo('') === 'Este campo debe tener al menos 1 caracter',
         'validar descripción no validó que la descripción sea vacía'
     );
 
     console.assert(
-        validarDescripcion('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') === 'Este campo debe tener menos de 100 caracteres',
+        validarDescripcionRegalo('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') === 'Este campo debe tener menos de 100 caracteres',
         'validar descripción no validó que la descripción sea menor a 100 caracteres'
+    );
+
+    console.assert(
+        validarDescripcionRegalo(',.,.') === 'Este campo sólo puede tener letras y números',
+        'validar descripción no validó que la descripción contenga sólo letras y números'
+    );
+
+    console.assert(
+        validarDescripcionRegalo('Regalo') === '',
+        'validar descripción no validó que la descripción sea correcta'
     );
 }
 
 probarValidarNombre();
 probarValidarCiudad();
-probarValidarDescripcion();
+probarValidarDescripcionRegalo();
